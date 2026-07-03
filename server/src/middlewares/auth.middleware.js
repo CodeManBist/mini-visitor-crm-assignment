69fc4a5ae5f39834ed7791600d3639107022456d
CodeManBist <sagarbist372@gmail.com>
2026-07-04 00:32:24 +0530

    fixed auth middleware

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,35 +1,37 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-const protect = (req, res, next) => {
+const protect = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
 
-    try {
-
-        const token = req.headers.authorization?.split(" ")[1];
-
-        if (!token) {
-
-            return res.status(401).json({
-                success: false,
-                message: "Unauthorized"
-            });
-
-        }
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        req.user = decoded;
-
-        next();
-
-    } catch (error) {
-
-        return res.status(401).json({
-            success: false,
-            message: "Invalid Token"
-        });
-
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
     }
 
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid Token",
+    });
+  }
 };
 
 export default protect;
